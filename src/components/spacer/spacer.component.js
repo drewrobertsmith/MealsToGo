a21ec3dd61873bdfe665e7f05a7f31a82de1b3c4
drewrobertsmith <drew.robert.smith@gmail.com>
2023-02-06 10:55:29 -0600

    Getting variant of styled component ahead of time so android stops crashing on load

--- src/components/spacer/spacer.component.js
+++ src/components/spacer/spacer.component.js
@@ -1,30 +1,42 @@
-import styled from "styled-components/native";
+import React from "react";
+import styled, { useTheme } from "styled-components/native";
 
 const sizeVariant = {
-  small: 1,
-  medium: 2,
-  large: 3,
+    small: 1,
+    medium: 2,
+    large: 3,
 };
 
 const positionVariant = {
-  top: "marginTop",
-  left: "marginLeft",
-  right: "marginRight",
-  bottom: "marginBottom",
+    top: "marginTop",
+    left: "marginLeft",
+    right: "marginRight",
+    bottom: "marginBottom",
 };
 
 const getVariant = (position, size, theme) => {
-  const sizeIndex = sizeVariant[size];
-  const property = positionVariant[position];
-  const value = theme.space[sizeIndex];
-  return `${property}:${value}`;
+    const sizeIndex = sizeVariant[size];
+    const property = positionVariant[position];
+    const value = theme.space[sizeIndex];
+    return `${property}:${value}`;
 };
 
-export const Spacer = styled.View`
-  ${({ position, size, theme }) => getVariant(position, size, theme)}
+const SpacerView = styled.View`
+    ${({ variant }) => variant};
 `;
 
+export const Spacer = ({ position, size, children }) => {
+    const theme = useTheme();
+    const variant = getVariant(position, size, theme);
+    return (
+        <SpacerView variant={variant}>
+            {children}
+        </SpacerView>
+    )
+}
+
+
 Spacer.defaultProps = {
-  position: "top",
-  size: "small",
+    position: "top",
+    size: "small",
 };
