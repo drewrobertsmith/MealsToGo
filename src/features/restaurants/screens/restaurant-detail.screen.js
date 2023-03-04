0266f96d6edd97e6bd44c1bcd04ffbf783d55be9
drewrobertsmith <drew.robert.smith@gmail.com>
2023-03-04 10:49:58 -0600

    Added MapView to new Map Feature Screen

--- src/features/restaurants/screens/restaurant-detail.screen.js
+++ src/features/restaurants/screens/restaurant-detail.screen.js
@@ -5,78 +5,78 @@ import { List } from "react-native-paper";
 import { ScrollView } from "react-native";
 
 export const RestaurantDetailScreen = ({ route }) => {
-    const { restaurant } = route.params;
-    const [expanded, setExpanded] = useState(true);
-    const handlePress = () => setExpanded(!expanded);
+  const { restaurant } = route.params;
+  const [expanded, setExpanded] = useState(true);
+  const handlePress = () => setExpanded(!expanded);
 
-    return (
-        <SafeArea>
-            <RestaurantInfoCard restaurant={restaurant} />
-            <ScrollView>
-                <List.Accordion
-                    title="Breakfast"
-                    left={(props) => <List.Icon {...props} icon="bread-slice" />}
-                    onPress={handlePress}
-                >
-                    <List.Item
-                        title="Burrito"
-                        description="Elit incididunt duis esse elit."
-                    />
-                    <List.Item
-                        title="Sandwich"
-                        description="Quis duis ipsum sit ea sit dolore officia ut elit."
-                    />
-                    <List.Item
-                        title="French Toast"
-                        description="Qui esse dolore qui incididunt sint dolor."
-                    />
-                </List.Accordion>
-                <List.Accordion
-                    title="Lunch"
-                    left={(props) => <List.Icon {...props} icon="hamburger" />}
-                    onPress={handlePress}
-                >
-                    <List.Item
-                        title="Buffalo Chicken"
-                        description="Esse exercitation veniam ea consequat sunt."
-                    />
-                    <List.Item
-                        title="Truffle BLT"
-                        description="Minim velit ullamco ad veniam ad enim qui proident eu."
-                    />
-                    <List.Item
-                        title="Rosemary Fries"
-                        description="Veniam excepteur adipisicing eiusmod sint deserunt duis elit aliqua consequat nulla sunt nisi."
-                    />
-                </List.Accordion>
-                <List.Accordion
-                    title="Dinner"
-                    left={(props) => <List.Icon {...props} icon="food-steak" />}
-                    onPress={handlePress}
-                >
-                    <List.Item
-                        title="Steak"
-                        description="Lorem deserunt quis amet voluptate excepteur cupidatat voluptate tempor ex sit enim."
-                    />
-                    <List.Item
-                        title="Potatoes"
-                        description="Laborum aliquip laborum commodo occaecat eiusmod Lorem qui."
-                    />
-                    <List.Item
-                        title="Fried Chicken"
-                        description="Do et duis esse anim sint commodo anim deserunt ex sint nisi."
-                    />
-                </List.Accordion>
-                <List.Accordion
-                    title="Drinks"
-                    left={(props) => <List.Icon {...props} icon="cup" />}
-                    onPress={handlePress}
-                >
-                    <List.Item title="Old Fashioned" />
-                    <List.Item title="Liquid Death" />
-                    <List.Item title="Milk" />
-                </List.Accordion>
-            </ScrollView>
-        </SafeArea>
-    );
+  return (
+    <SafeArea>
+      <RestaurantInfoCard restaurant={restaurant} />
+      <ScrollView>
+        <List.Accordion
+          title="Breakfast"
+          left={(props) => <List.Icon {...props} icon="bread-slice" />}
+          onPress={handlePress}
+        >
+          <List.Item
+            title="Burrito"
+            description="Elit incididunt duis esse elit."
+          />
+          <List.Item
+            title="Sandwich"
+            description="Quis duis ipsum sit ea sit dolore officia ut elit."
+          />
+          <List.Item
+            title="French Toast"
+            description="Qui esse dolore qui incididunt sint dolor."
+          />
+        </List.Accordion>
+        <List.Accordion
+          title="Lunch"
+          left={(props) => <List.Icon {...props} icon="hamburger" />}
+          onPress={handlePress}
+        >
+          <List.Item
+            title="Buffalo Chicken"
+            description="Esse exercitation veniam ea consequat sunt."
+          />
+          <List.Item
+            title="Truffle BLT"
+            description="Minim velit ullamco ad veniam ad enim qui proident eu."
+          />
+          <List.Item
+            title="Rosemary Fries"
+            description="Veniam excepteur adipisicing eiusmod sint deserunt duis elit aliqua consequat nulla sunt nisi."
+          />
+        </List.Accordion>
+        <List.Accordion
+          title="Dinner"
+          left={(props) => <List.Icon {...props} icon="food-steak" />}
+          onPress={handlePress}
+        >
+          <List.Item
+            title="Steak"
+            description="Lorem deserunt quis amet voluptate excepteur cupidatat voluptate tempor ex sit enim."
+          />
+          <List.Item
+            title="Potatoes"
+            description="Laborum aliquip laborum commodo occaecat eiusmod Lorem qui."
+          />
+          <List.Item
+            title="Fried Chicken"
+            description="Do et duis esse anim sint commodo anim deserunt ex sint nisi."
+          />
+        </List.Accordion>
+        <List.Accordion
+          title="Drinks"
+          left={(props) => <List.Icon {...props} icon="cup" />}
+          onPress={handlePress}
+        >
+          <List.Item title="Old Fashioned" />
+          <List.Item title="Liquid Death" />
+          <List.Item title="Milk" />
+        </List.Accordion>
+      </ScrollView>
+    </SafeArea>
+  );
 };
--- src/infrastructure/navigation/app.navigator.js
+++ src/infrastructure/navigation/app.navigator.js
@@ -6,6 +6,7 @@ import { Ionicons } from "@expo/vector-icons";
 import { Text } from "react-native";
 import { SafeArea } from "../../components/utility/safe-area.component";
 import { theme } from "../../infrastructure/theme";
+import { MapScreen } from "../../features/map/screens/map.screen";
 
 const Tab = createBottomTabNavigator();
 
@@ -23,14 +24,6 @@ const SettingsScreen = () => {
   );
 };
 
-const MapScreen = () => {
-  return (
-    <SafeArea>
-      <Text>Maps!</Text>
-    </SafeArea>
-  );
-};
-
 const createScreenOptions = ({ route }) => {
   const iconName = TAB_ICON[route.name];
   return {
@@ -44,9 +37,7 @@ const createScreenOptions = ({ route }) => {
 
 export const AppNavigator = () => (
   <NavigationContainer>
-    <Tab.Navigator
-      screenOptions={createScreenOptions}
-    >
+    <Tab.Navigator screenOptions={createScreenOptions}>
       <Tab.Screen name="Restaurants" component={RestaurantsNavigator} />
       <Tab.Screen name="Map" component={MapScreen} />
       <Tab.Screen name="Settings" component={SettingsScreen} />
