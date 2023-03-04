28285587caf66bc64523b082ec365e814b7983a1
drewrobertsmith <drew.robert.smith@gmail.com>
2023-03-04 11:44:39 -0600

    brought in location context, surfaced viewport data, calculating lat,lng, & latDelta as the location and viewport changes in the search bar

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import MapView from "react-native-maps";
 import styled from "styled-components/native";
+import { LocationContext } from "../../../services/location/location.context";
+import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
 import { Search } from "../components/search.component";
 
 const Map = styled(MapView)`
@@ -8,9 +10,36 @@ const Map = styled(MapView)`
   width: 100%;
 `;
 
-export const MapScreen = () => (
-  <>
-    <Search />
-    <Map />
-  </>
-);
+export const MapScreen = () => {
+    const { location } = useContext(LocationContext);
+    const { restaurants = [] } = useContext(RestaurantsContext);
+
+    const [latDelta, setLatDelta] = useState(0);
+
+    const { lat, lng, viewport } = location;
+
+    useEffect(() => {
+        const northeastLat = viewport.northeast.lat;
+        const southwestLat = viewport.southwest.lat;
+
+        setLatDelta(northeastLat - southwestLat);
+    }, [location, viewport]);
+
+    return (
+        <>
+            <Search />
+            <Map
+                region={{
+                    latitude: lat,
+                    longitude: lng,
+                    latitudeDelta: latDelta,
+                    longitudeDelta: 0.02,
+                }}
+            >
+                {restaurants.map(() => {
+                    return null;
+                })}
+            </Map>
+        </>
+    )
+};
