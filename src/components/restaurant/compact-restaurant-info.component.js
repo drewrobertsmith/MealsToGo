52614cb07cf1fc871e5c9663a1eff768602537de
drewrobertsmith <drew.robert.smith@gmail.com>
2023-03-11 09:56:38 -0600

    added a custom callout view to each marker to render the restaurant image and title, while accounting for a standard image for iOS and a webview on android

---
+++ src/components/restaurant/compact-restaurant-info.component.js
@@ -0,0 +1,39 @@
+import React from "react";
+import styled from "styled-components/native";
+import WebView from "react-native-webview";
+import { Text } from "../typography/text.component";
+import { Platform } from "react-native";
+
+const CompactImage = styled.Image`
+  border-radius: 10px;
+  width: 120px;
+  height: 100px;
+`;
+
+const Item = styled.View`
+    padding: 10px;
+    max-width: 120px
+    align-items: center;
+`;
+
+//Android needs a webview to render the image rather than just a view on the callout
+const CompactWebView = styled(WebView)`
+  border-radius: 10px;
+  width: 120px;
+  height: 100px;
+`;
+
+//check if on android
+const isAndroid = Platform.OS === "android";
+export const CompactRestaurantInfo = ({ restaurant }) => {
+    //if on android show webview, otherwise view cuz android madness
+    const Image = isAndroid ? CompactWebView : CompactImage;
+    return (
+        <Item>
+            <Image source={{ uri: restaurant.photos[0] }} />
+            <Text center variant="caption" numberOfLines={3}>
+                {restaurant.name}
+            </Text>
+        </Item>
+    );
+};
--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -1,9 +1,9 @@
 import React, { useContext, useState, useEffect } from "react";
-import MapView, { Marker } from "react-native-maps";
+import MapView, { Marker, Callout } from "react-native-maps";
 import styled from "styled-components/native";
-
 import { LocationContext } from "../../../services/location/location.context";
 import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
+import { MapCallout } from "../components/map-callout.component";
 
 import { Search } from "../components/search.component";
 
@@ -13,44 +13,48 @@ const Map = styled(MapView)`
 `;
 
 export const MapScreen = () => {
-    const { location } = useContext(LocationContext);
-    const { restaurants = [] } = useContext(RestaurantsContext);
+  const { location } = useContext(LocationContext);
+  const { restaurants = [] } = useContext(RestaurantsContext);
 
-    const [latDelta, setLatDelta] = useState(0);
+  const [latDelta, setLatDelta] = useState(0);
 
-    const { lat, lng, viewport } = location;
+  const { lat, lng, viewport } = location;
 
-    useEffect(() => {
-        const northeastLat = viewport.northeast.lat;
-        const southwestLat = viewport.southwest.lat;
+  useEffect(() => {
+    const northeastLat = viewport.northeast.lat;
+    const southwestLat = viewport.southwest.lat;
 
-        setLatDelta(northeastLat - southwestLat);
-    }, [location, viewport]);
+    setLatDelta(northeastLat - southwestLat);
+  }, [location, viewport]);
 
-    return (
-        <>
-            <Search />
-            <Map
-                region={{
-                    latitude: lat,
-                    longitude: lng,
-                    latitudeDelta: latDelta,
-                    longitudeDelta: 0.02,
-                }}
+  return (
+    <>
+      <Search />
+      <Map
+        region={{
+          latitude: lat,
+          longitude: lng,
+          latitudeDelta: latDelta,
+          longitudeDelta: 0.02,
+        }}
+      >
+        {restaurants.map((restaurant) => {
+          return (
+            <Marker
+              key={restaurant.name}
+              title={restaurant.name}
+              coordinate={{
+                latitude: restaurant.geometry.location.lat,
+                longitude: restaurant.geometry.location.lng,
+              }}
             >
-                {restaurants.map((restaurant) => {
-                    return (
-                        <Marker
-                            key={restaurant.name}
-                            title={restaurant.name}
-                            coordinate={{
-                                latitude: restaurant.geometry.location.lat,
-                                longitude: restaurant.geometry.location.lng,
-                            }}
-                        />
-                    );
-                })}
-            </Map>
-        </>
-    );
+              <Callout>
+                <MapCallout restaurant={restaurant} />
+              </Callout>
+            </Marker>
+          );
+        })}
+      </Map>
+    </>
+  );
 };
