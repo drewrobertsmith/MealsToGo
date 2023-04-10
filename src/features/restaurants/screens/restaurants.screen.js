2966afa158d033eb4abbe40e142bf07572be7189
drewrobertsmith <drew.robert.smith@gmail.com>
2023-04-10 11:47:01 -0500

    Revert "Initial Commit"
    This reverts commit f253cf8c484e14fac0315d73aa83f5253011be4b.

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -52,7 +52,6 @@ export const RestaurantsScreen = ({ navigation }) => {
         data={restaurants}
         renderItem={({ item }) => {
           return (
-            //TODO: change out the touchable opacity to pressable API so there isnt a weird box underlay
             <TouchableOpacity
               onPress={() =>
                 navigation.navigate("Restaurant Detail", {
